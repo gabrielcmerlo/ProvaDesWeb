0acaa8e83b31f559ec9291ba4f0f28b014bcf47a
gabrielcmerlo <gabrielcm2203@gmail.com>
2024-11-28 20:15:56 -0300

    Primeiro commit

---
+++ prova/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="App">
+      <div className="App-header">
+      <header>
+        MyFinance
+      <span><a className="App-link" href="#home">Home</a></span>
+      <span><a className="App-link" href="#transacoes">Transações</a></span>
+      </header>
+      </div>
+      <div className="App-body">
+        <p className="App-phome">Home</p>
+        <p className="App-p">Total Entradas: 6350</p>
+        <p className="App-p">Total Saídas: 4140</p>
+        <p className="App-p">Saldo Atual: 2210</p>
+      </div>
+      <div className="footer">
+        Desenvolvido por Gabriel Costa Merlo
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
